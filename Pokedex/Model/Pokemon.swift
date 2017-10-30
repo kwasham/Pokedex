bd00d2d3161ae30e8a8c355da1dc8a6d5de86b6f
kwasham <kawasham@att.net>
2017-10-29 23:28:01 -0500

    Parsing CSV and Collection View Working

---
+++ Pokedex/Model/Pokemon.swift
@@ -0,0 +1,32 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Kirk Washam on 10/29/17.
+//  Copyright © 2017 StudioATX. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+        
+    }
+    
+    
+}
